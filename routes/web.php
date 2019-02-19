604e0d7209607e8a4532172621a404bb3c77c61c
root <root@localhost.localdomain>
2019-02-19 18:29:58 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('cart/add', 'CartController@addGoods2');
+Route::get('redis/list', 'CartController@redisList');
+Route::get('redis/add', 'CartController@redisAdd');
+Route::get('redis/del', 'CartController@redisDel');
+
+
+
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', 'AdminController@index');
+    Route::get('/index', 'AdminController@index');
+});
