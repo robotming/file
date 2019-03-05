c752bdd9be81ae6d18dd2af08302835f964f2341
zhangrui <root@localhost.localdomain>
2019-03-05 15:09:46 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,17 @@
 
 Route::get('/', "IndexController@index");
 
-
+//
 Route::get('cart/add', 'CartController@addGoods2');
 Route::get('redis/list', 'CartController@redisList');
 Route::get('redis/add', 'CartController@redisAdd');
 Route::get('redis/del', 'CartController@redisDel');
-
+//
 
 
 
 Route::prefix('admin')->group(function () {
     Route::get('/', 'AdminController@index');
     Route::get('/index', 'AdminController@index');
+    Route::get('/test', 'AdminController@test');
 });
